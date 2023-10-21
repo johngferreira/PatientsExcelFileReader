c5870371efcb9975ec9b9061f31fb21437d5b37a
johng <johng@LAPTOP-I601G42F>
2023-10-21 11:24:20 -0400

    Add project files.

---
+++ Patients/Patients.cs
@@ -0,0 +1,10 @@
+﻿namespace PatientsExcelFileReader.Patients
+{
+    public class Patients
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+    }
+}
